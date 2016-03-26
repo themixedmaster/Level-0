f34a2032986d6ee6a244dd36e360f70391062fd6
Gavin Morris <themixedmaster@gmail.com>
2016-03-26 10:06:51 -0700

    week4ithink

---
+++ src/RocketShipBlastoff.java
@@ -0,0 +1,20 @@
+
+public class RocketShipBlastoff {
+
+	public static void main(String[] args) {
+		
+		
+		for(int count = 100;count > 0; count--){
+			
+		System.out.println( count );
+		}
+		System.out.println(" Beep Beep Beep- BOOOOOOOOOOOOOMMM!! Congradulations, the bomb exploded, everyone's dead!");
+		
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/TortoiseColorChooser.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+
+import org.teachingextensions.logo.Colors;
+
+import org.teachingextensions.logo.Tortoise;
+
+
+public class TortoiseColorChooser {
+
+    public static void main(String[] args) {
+
+
+        //3. ask the user what color they would like the tortoise to draw
+
+        
+
+        //4. use an if/else statement to set the pen color that the user requested
+
+
+//5. if the user doesn’t enter anything, choose a random color
+
+
+//6. put a loop around your code so that you keep asking the user for more colors & drawing them
+
+        
+
+        //2. set the pen width to 10
+
+        
+
+    //1. make the tortoise draw a shape (this will take more than one line of code)
+
+
+
+    }
+
+}
