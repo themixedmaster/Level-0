fefad98e6fe32416246e146f75023aef7b7ea581
Gavin Morris <themixedmaster@gmail.com>
2016-04-02 10:02:19 -0700

    week 5

--- src/TortoiseColorChooser.java
+++ src/TortoiseColorChooser.java
@@ -2,6 +2,7 @@ import javax.swing.JOptionPane;
 
 import java.awt.Color;
 
+
 import org.teachingextensions.logo.Colors;
 
 import org.teachingextensions.logo.Tortoise;
@@ -10,14 +11,21 @@ import org.teachingextensions.logo.Tortoise;
 public class TortoiseColorChooser {
 
     public static void main(String[] args) {
-
-
+    	
+ Tortoise.show();
         //3. ask the user what color they would like the tortoise to draw
-
-        
-
+  		String red = JOptionPane.showInputDialog("How much red?");
+  		System.out.print( red );
+  		if (red.equals("")) {
+	System.out.print("cvhdfhc");
+}
+  		int truered = Integer.parseInt(red);
+  		String lime = JOptionPane.showInputDialog("How much lime?");
+  		int truelime = Integer.parseInt(lime);
+  		String blue = JOptionPane.showInputDialog("How much blue?");
+  		int trueblue = Integer.parseInt(blue);
         //4. use an if/else statement to set the pen color that the user requested
-
+     Tortoise.setPenColor(new Color(truered,truelime,trueblue));
 
 //5. if the user doesn’t enter anything, choose a random color
 
@@ -27,10 +35,769 @@ public class TortoiseColorChooser {
         
 
         //2. set the pen width to 10
-
+Tortoise.setPenWidth(10);
         
 
     //1. make the tortoise draw a shape (this will take more than one line of code)
+int steps = 10;
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
+
+Tortoise.move(steps);
+Tortoise.turn(90);
+steps = steps + 10;
 
 
 
--- src/bdayreminder.java
+++ src/bdayreminder.java
@@ -1,4 +1,62 @@
+import javax.swing.JOptionPane;
 
 public class bdayreminder {
 
+	public static void main(String[] args) {
+
+	    
+
+        // 1. correct the birthdays for your family below
+
+        String momsBirthday = "April 26th";
+
+        String dadsBirthday = "March 30th";
+
+        String myBirthday = "December 22nd";
+        
+        String dogsBirthday = "November I think";
+        
+        String owensBirthday = "May 28th";
+        
+        String dylansBirthday = "December 7th";
+
+
+        // 2. Find out which birthday the user wants and and store their response in a variable
+        String familymember = JOptionPane.showInputDialog(" Who's birthday do you want to remember?");
+    
+
+        // 3. Print out what the user typed
+      
+        System.out.print( familymember );
+        
+        // 4. if user asked for "mom"
+        	if( familymember.equals("Gavin")){
+        	System.out.print("'s birthday is " + myBirthday + ".");	
+        	}
+            //print mom's birthday
+        	else if( familymember.equals("Dad")){
+            	System.out.print("'s birthday is " + dadsBirthday + ".");	
+            	}
+        // 5. if user asked for "dad"
+        	else if( familymember.equals("Mom")){
+            	System.out.print("'s birthday is " + momsBirthday + ".");	
+            	}
+            // print dad's birthday
+        	else if( familymember.equals("Baloo")){
+            	System.out.print("'s birthday is " + dogsBirthday + ".");	
+            	}
+        // 6. if user asked for your name
+        	else if( familymember.equals("Owen")){
+            	System.out.print("'s birthday is " + owensBirthday + ".");	
+            	}
+            // print myBirthday
+        	else if( familymember.equals("Dylan")){
+            	System.out.print("'s birthday is " + dylansBirthday + ".");	
+            	}
+        //7. otherwise print "Sorry, i don't remember that person's birthday!"
+        	else{
+            	System.out.print(", who's " + familymember + "?");	
+            	}
+    } 
+	
 }
